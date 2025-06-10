f3c1fae56dcd542e1d1d56c53cda39a95a2c0c4e
Jay Zende <jay@Jays-MacBook-Pro.local>
2025-06-10 17:11:14 +0530

    Dummy commit

--- actions/export-secrets/main.sh
+++ actions/export-secrets/main.sh
@@ -2,16 +2,11 @@
 
 SECRETS_FILE="$1"
 APP_NAME="$2"
-SERVICE_NAME="$3" 
+SERVICE_NAME="$3"
 VAULT_ENV_KEYS="$4"
 
-
 while IFS=: read -r key; do
- 
-  SECRET_NAME="${APP_NAME}-${SERVICE_NAME}-${key}"
-  secret_value=$(gcloud secrets versions access latest --secret="${SECRET_NAME}") 
-
-  echo "${key}: ${secret_value}" >> "$SECRETS_FILE"
-
-done < "$VAULT_ENV_KEYS"
-
+    SECRET_NAME="${APP_NAME}-${SERVICE_NAME}-${key}"
+    secret_value=$(gcloud secrets versions access latest --secret="$SECRET_NAME")
+    echo "${key}: ${secret_value}" >> "$SECRETS_FILE"
+done < "$VAULT_ENV_KEYS" 
