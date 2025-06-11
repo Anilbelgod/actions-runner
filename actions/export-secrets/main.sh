8fe1ef9345124c809c2f3feeaf9bf2d5a2a18a03
Jay Zende <jay@Jays-MacBook-Pro.local>
2025-06-11 14:09:47 +0530

    Updated export secret main bash

--- actions/export-secrets/main.sh
+++ actions/export-secrets/main.sh
@@ -6,9 +6,9 @@ SERVICE_NAME="$3"
 VAULT_ENV_KEYS="$4"
 
 while IFS=: read -r line; do
-    key=$(echo "$line" | sed 's/^- *//')
+    key=$(echo "$line" | sed 's/^[[:space:]]*-[[:space:]]*//')
     SECRET_NAME="${APP_NAME}-${SERVICE_NAME}-${key}"
     secret_value=$(gcloud secrets versions access latest --secret="$SECRET_NAME")
-    echo "${key}: ${secret_value}" >> "$SECRETS_FILE"
+    echo "${key}:${secret_value}" >> "$SECRETS_FILE"
 done < "$VAULT_ENV_KEYS" 
 
