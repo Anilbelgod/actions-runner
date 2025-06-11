ea1beb9c7df62aa0f53bf070fd0ae713d398becb
Jay Zende <jay@Jays-MacBook-Pro.local>
2025-06-11 13:47:24 +0530

    Printing vault keys

--- actions/export-secrets/main.sh
+++ actions/export-secrets/main.sh
@@ -5,8 +5,11 @@ APP_NAME="$2"
 SERVICE_NAME="$3"
 VAULT_ENV_KEYS="$4"
 
-while IFS=: read -r key; do
-    SECRET_NAME="${APP_NAME}-${SERVICE_NAME}-${key}"
-    secret_value=$(gcloud secrets versions access latest --secret="$SECRET_NAME")
-    echo "${key}: ${secret_value}" >> "$SECRETS_FILE"
+while IFS=: read -r line; do
+    key=$(echo "$line" | sed 's/^- *//')
+    # SECRET_NAME="${APP_NAME}-${SERVICE_NAME}-${key}"
+    # secret_value=$(gcloud secrets versions access latest --secret="$SECRET_NAME")
+    # echo "${key}: ${secret_value}" >> "$SECRETS_FILE"
+    echo "Printing keys:"
+    echo $key 
 done < "$VAULT_ENV_KEYS" 
