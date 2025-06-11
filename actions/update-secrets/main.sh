eb4d48255566e8bf67e634820f3cc23fd581ac6b
Jay Zende <jay@Jays-MacBook-Pro.local>
2025-06-11 22:51:11 +0530

    Printing secrets file

--- actions/update-secrets/main.sh
+++ actions/update-secrets/main.sh
@@ -5,22 +5,24 @@ APP_NAME="$2"
 SERVICE_NAME="$3" 
 TEMP_SECRET_FILE="$4"  
 
-echo "Printing secrets file: $SECRETS_FILE" 
+# echo "Printing secrets file: $SECRETS_FILE" 
 
 
-# while IFS=: read -r key value; do
+while IFS=: read -r key value; do
  
-#   SECRET_NAME="${APP_NAME}-${SERVICE_NAME}-${key}"
+  SECRET_NAME="${APP_NAME}-${SERVICE_NAME}-${key}"
   
-
-#   if ! gcloud secrets describe "$SECRET_NAME" >/dev/null 2>&1; then
-#     echo "Creating secret: $SECRET_NAME"
-#     gcloud secrets create "$SECRET_NAME"  
-#   fi 
-
-#   echo "$value" > $TEMP_SECRET_FILE  
-#   echo "Adding version to secret: $SECRET_NAME"
-#   gcloud secrets versions add "$SECRET_NAME" --data-file=$TEMP_SECRET_FILE 
+  echo "Secret name: $key" 
 
 
-# done < "$SECRETS_FILE" 
+  # if ! gcloud secrets describe "$SECRET_NAME" >/dev/null 2>&1; then
+  #   echo "Creating secret: $SECRET_NAME"
+  #   gcloud secrets create "$SECRET_NAME"  
+  # fi 
+
+  # echo "$value" > $TEMP_SECRET_FILE  
+  # echo "Adding version to secret: $SECRET_NAME"
+  # gcloud secrets versions add "$SECRET_NAME" --data-file=$TEMP_SECRET_FILE 
+
+
+done < "$SECRETS_FILE" 
