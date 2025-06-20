b0cadfbb150be4d8c997c53dfe41b14469a82c6e
Jay Zende <jay@Jays-MacBook-Pro.local>
2025-06-20 16:07:05 +0530

    Edited secret deletion step

--- actions/delete-secrets/main.sh
+++ actions/delete-secrets/main.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 APP_NAME="$1"
-SERVICE_NAME="$2"
+ENVIRONMENT="$2"
 DELETE_ALL="$3"
 DELETE_ALL_EXCEPT_KEYS="$4"   # Path to file with keys like: - region
 TEMP_SECRETS_FILE="$5"        # Path to file with full secret names
@@ -9,12 +9,9 @@ TEMP_SECRETS_FILE="$5"        # Path to file with full secret names
 GCS_BUCKET_PATH="gs://cch-cicd-test-bucket/temp"
 
 gcloud secrets list \
---filter="name~'${APP_NAME}-${SERVICE_NAME}-.*'" \
+--filter="name~'service-${APP_NAME}-secret-${ENVIRONMENT}-.*'" \
 --format="value(name)" > "$TEMP_SECRETS_FILE" 
 
-# Backup input files to GCS (optional but useful for debugging)
-gsutil cp "$TEMP_SECRETS_FILE" "$GCS_BUCKET_PATH"
-gsutil cp "$DELETE_ALL_EXCEPT_KEYS" "$GCS_BUCKET_PATH"
 
 # Deletion Logic
 if [ ! -z "$DELETE_ALL" ] && [ "$DELETE_ALL" == "true" ]; then
@@ -41,7 +38,7 @@ elif [ -f "$DELETE_ALL_EXCEPT_KEYS" ]; then
 
         while IFS= read -r line; do
             key=$(echo "$line" | sed 's/^[[:space:]]*-[[:space:]]*//')
-            SECRET_NAME="${APP_NAME}-${SERVICE_NAME}-${key}"
+            SECRET_NAME="service-${APP_NAME}-secret-${ENVIRONMENT}-${key}"
 
             echo "Comparing: $SECRET_NAME with $existing_key"
             if [ "$SECRET_NAME" == "$existing_key" ]; then
