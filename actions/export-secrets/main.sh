2805e6830ce02a67f46b6c87c3c9e22f36dcb92e
Jay Zende <jay@Jays-MacBook-Pro.local>
2025-06-23 19:08:25 +0530

    Updated secrets step

--- actions/export-secrets/main.sh
+++ actions/export-secrets/main.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-APP_NAME="$1"
-ENVIRONMENT="$2"
-VAULT_ENV_KEYS="$3"
-SECRETS_FILE="$4"
+SECRET_SM_PATH="$1"
+VAULT_ENV_KEYS="$2"
+SECRETS_FILE="$3"
 
 while IFS=: read -r line; do
     #Remove leading and trailing whitespace
     key=$(echo "$line" | sed 's/^[[:space:]]*-[[:space:]]*//')
 
-    SECRET_NAME="service-${APP_NAME}-secret-${ENVIRONMENT}-${key}"
+    SECRET_NAME="${SECRET_SM_PATH}-${key}"
 
     secret_value=$(gcloud secrets versions access latest --secret="$SECRET_NAME")
 
