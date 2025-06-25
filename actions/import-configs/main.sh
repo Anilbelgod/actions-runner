0f3e8dc1cbbc6bb8d6cb0fb4e13ae2225c2d753f
Jay Zende <jay@Jays-MacBook-Pro.local>
2025-06-25 14:35:24 +0530

    changed echo statement

--- actions/import-configs/main.sh
+++ actions/import-configs/main.sh
@@ -25,9 +25,8 @@ for CONFIGS in $CONFIG_NAMES; do
     --secret="$CONFIGS" \
     --quiet | tr -d '\n')
 
-  echo "${CONFIG_KEY}: ${SECRET_VALUE}" >> "$CONFIGS_FILE"
-  # Append the key-value pair to the YAML file using yq
-#   yq eval -i ".${CONFIG_KEY} = \"${SECRET_VALUE}\"" "$CONFIGS_FILE"
+#   echo "${CONFIG_KEY}: ${SECRET_VALUE}" >> "$CONFIGS_FILE"
+    yq eval -i ".${CONFIG_KEY} = (${SECRET_VALUE} | from_json)" "$CONFIGS_FILE"
 done
 
 echo "Secrets successfully written to $CONFIGS_FILE"
